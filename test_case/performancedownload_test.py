5eecfe0cb1ae7ccc5849354a2b871d30821df9a9
min120 <wh_2002_cn@163.com>
2018-04-26 16:00:35 +0800

    change some code for large files

--- test_case/performancedownload_test.py
+++ test_case/performancedownload_test.py
@@ -10,10 +10,10 @@ import random
 
 timeout =20
 socket.setdefaulttimeout(timeout)#这里对整个socket层设置超时时间。后续文件中如果再使用到socket，不必再设置
-sleep_download_time = 0.1
+sleep_download_time = 0.4
 def test_video_download():
     num= random.randint(1,10000)
-    headers= {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.%d (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'%num}
+    headers= {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.08 (KHTML, like Gecko) Chrome/%d.0.3359.117 Safari/537.36'%num}
    # headers = {'nb-file-md5': md5code}
     #print(headers)
     file_id ='efef4642-160b-4404-aa94-6f2930773814'
@@ -22,24 +22,28 @@ def test_video_download():
     DataAll1 = {'headers':headers,'params': params}
 
     r1 = HttpService.MyHTTP(url1).nojsonget(url1, **DataAll1)
-    date1 = r1.headers
-    print(date1['Content-Disposition'])
-    r1_content = date1['Content-Disposition']
-    bool_endwith1 = r1_content.endswith('.mp4')
-    print(bool_endwith1)
-    r1.close()
+    while True:
+        line = r1.readline()
+        if not line:
+            break
 
+    #date1 = r1.headers
+    #print(date1['Content-Disposition'])
+    #r1_content = date1['Content-Disposition']
+    #bool_endwith1 = r1_content.endswith('.mp4')
+    #print(bool_endwith1)
+    r1.close()
 
 try:
     i = 0
     tasks = []  # 任务列表
-    task_number = 1500
+    task_number = 1
     while i < task_number:
         time.sleep(sleep_download_time)
         t = threading.Thread(target=test_video_download)
         tasks.append(t)  # 加入线程池，按需使用
         t.start()  # 多线程并发
         i+=1
-        print(i)
+        #print(i)
 except Exception as e:
     print(e)
