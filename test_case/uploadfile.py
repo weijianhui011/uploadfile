7318b70c4003fbed847a107e3b6e6cb496b04fe4
weijianhui011 <newwjh@163.com>
2018-04-28 15:43:28 +0800

    优化代码

--- test_case/uploadfile.py
+++ test_case/uploadfile.py
@@ -1,51 +1,33 @@
-
 import unittest
 import os
 from public import HttpService
 from public import base
 from public import Config
-from ddt import ddt,data,unpack
-
-testcasefile = 'get_params_headers_data.xlsx'
-AllData = base.get_data(testcasefile,'AllData')
-print(AllData)
-TestData = base.get_data(testcasefile,'TestData')
-Endpoint = AllData[1][1]
-dataall=TestData[1][2]
-expectresult=TestData[1][3]
-filelist= base.get_data(testcasefile,'filelist')
-filefortest = filelist[1:]
-filepath = Config.uploadpath()
-print('@@@@@@@@@@@@')
-print(filefortest)
-print()
+from ddt import ddt, data, unpack
 
 
-@ddt #装饰器在使用前需要将ddt给类使用，!!如果引入的ddt，需要@ddt.ddt
+@ddt  # 装饰器在使用前需要将ddt给类使用，!!如果引入的ddt，需要@ddt.ddt
 class PostParams(unittest.TestCase):
     def setUp(self):
-        endpoint='/nbfile/upload'
-        self.url = base.requesturl(endpoint)
-    @data (['1.txt'],['1.pdf'],['1.jpg'],['1.xlsx'])
+        endpoint = '/nbfile/upload'
+        self.url = base.request_url(endpoint)
+
+    @data (['1.txt'], ['1.pdf'], ['1.jpg'], ['1.xlsx'])
     @unpack
-    def test_post_params(self,filefortest1):
-        #上传一个文件判断是否上传成功
-         os.chdir(filepath)
-         filenameinfolder = filefortest1
-         md5code = base.getfilemd5(filenameinfolder)
-         headers = { 'nb-file-md5': md5code}
-         files = {'field1': (filenameinfolder, open(filenameinfolder, 'rb'))}
-         DataAll={'headers':headers, 'files':files }
-         r = HttpService.MyHTTP(self.url).post(self.url, **DataAll)
-         print(r)
-         connect = r.get('Success')
-         msg = r.get("Message")
-         print(connect)
-         self.assertEqual(connect,True)
-         self.assertEqual(msg,"上传成功")
-         files['field1'][1].close()
-
-
+    def test_post_params(self, file_for_test):
+        #  上传一个文件判断是否上传成功
+        os.chdir(Config.get_upload_path())
+        file_name_in_folder = file_for_test
+        md5code = base.get_file_md5(file_name_in_folder)
+        headers = { 'nb-file-md5': md5code}
+        files = {'field1': (file_name_in_folder, open(file_name_in_folder, 'rb'))}
+        data_all = {'headers': headers, 'files': files}
+        r = HttpService.MyHTTP(self.url).post(self.url, **data_all)
+        connect = r.get('Success')
+        msg = r.get("Message")
+        self.assertEqual(connect, True)
+        self.assertEqual(msg, "上传成功")
+        files['field1'][1].close()
 
     def tearDown(self):
         print('Test Over')
