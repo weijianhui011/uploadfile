317323e4b3dd0d71dfa3198b25648df239373424
weijianhui011 <newwjh@163.com>
2018-05-07 16:40:28 +0800

    修改report方法

--- run_test.py
+++ run_test.py
@@ -11,17 +11,10 @@ sys.path.append('./public')
 
 
 def report(test_report):  # 查找最新的测试报告
-    print(test_report)
-    cur_dir = os.curdir
-    print(cur_dir + "+++")
-    os.chdir(test_report)
-    print(cur_dir + "+++")
     lists = os.listdir(test_report)  # 返回指定的文件夹包含的文件或者文件夹的名字的列表
-    print(lists)
     lists.sort(key=lambda fn: os.path.getatime(test_report + "\\" + fn))  # 通过sort()方法重新按照时间对目录下的文件进行排序
     report_name = lists[-1]
-    print(report_name)
-    return  report_name
+    return "\\report\\" + report_name
 
 
 if __name__ == "__main__":
@@ -29,7 +22,7 @@ if __name__ == "__main__":
     # 指定测试用例为当前文件夹下的test_case目录
     # 自动化识别需要运行的测试用例
     test_dir = './test_case'
-    print(test_dir)
+    main_dir = os.getcwd()
     discover = unittest.defaultTestLoader.discover(test_dir, pattern='*test.py')
     now = time.strftime("%Y-%m-%d %H_%M_%S")  # 定义当前的时间戳
     filename = './report/' + now + '_result.html'  # 定义文件名
@@ -42,8 +35,6 @@ if __name__ == "__main__":
     )
     runner.run(discover)
     fp.close()
-    #os.chdir('.')
-    test_reports = './report'  # 定义报告文件目录
-    print(test_reports)
-    rep = report(test_reports)
+    test_reports = main_dir + '\\report\\'  # 定义报告文件目录
+    rep = main_dir + report(test_reports)
     mail.send_email(rep)
